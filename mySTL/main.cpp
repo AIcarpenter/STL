8be653b433a25397c12b5357112e47539875e849
moon <74983228@qq.com>
2016-08-30 15:05:43 +0800

    heap----finished 只是几个函数而已

--- mySTL/main.cpp
+++ mySTL/main.cpp
@@ -1,21 +1,31 @@
-//#include<vector>
-//#include<iostream>
-//#include<list>
-//#include<algorithm>
-//using namespace std;
-//
-//class A {
-//private:
-//	int i = 10;
-//public:
-//	int operator*()
-//	{
-//		return i;
-//	}
-//	A& operator->() {return & }
-//};
-//int main()
-//{
-//	A a;
-//	*a;
-//}
+#include"moonvector.h"
+#include<vector>
+#include<iostream>
+#include<list>
+#include<algorithm>
+#include<queue>
+#include<allocators>
+#include"heap.h"
+using namespace std;
+
+class A {
+private:
+	int a;
+public:
+	A() {}
+	A(int x) :a(x) {}
+	~A() {}
+	A(const A& other) { cout << "copy-constructor invocated." << endl; }
+	A& operator=(const A& other) { cout << "copy-assignment constructor invocated" << endl; return *this; }
+};
+
+int main()
+{
+	int ia[9] = { 5,2,5,-2,4,5,1,23,-9 };
+	vector<int> ivec(ia, ia + 9);
+	moon::make_heap(ivec.begin(), ivec.end()-1);
+	moon::sort_heap(ivec.begin(), ivec.end());
+	getchar();
+
+
+}
